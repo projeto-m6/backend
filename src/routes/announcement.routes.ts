5465db14af1f83201b83e8b992366c2065a3ed86
Rodrigo Nunes <imrodrigon@gmail.com>
2023-02-22 23:07:00 -0400

    feat: add middleware on route stack

--- src/routes/announcement.routes.ts
+++ src/routes/announcement.routes.ts
@@ -1,22 +1,30 @@
-import { Router } from "express";
-import { createAnnouncementController } from "../controllers/announcements/createAnnouncement.controller";
-import { deleteAnnouncementController } from "../controllers/announcements/deleteAnnouncement.constroller";
-import { readAnnouncementController } from "../controllers/announcements/readAnnouncement.controller";
-import { readAnnouncementIdController } from "../controllers/announcements/readAnnouncementId.controller";
-import { readAnnouncementUserController } from "../controllers/announcements/readAnnouncementUser.controller";
-import { updateAnnouncementController } from "../controllers/announcements/updateAnnouncement.controller";
-import verifyAuthTokenMiddleware from "../middlewares/verifyAuthToken.middleware";
+import { Router } from 'express';
+import { createAnnouncementController } from '../controllers/announcements/createAnnouncement.controller';
+import { deleteAnnouncementController } from '../controllers/announcements/deleteAnnouncement.constroller';
+import { readAnnouncementController } from '../controllers/announcements/readAnnouncement.controller';
+import { readAnnouncementIdController } from '../controllers/announcements/readAnnouncementId.controller';
+import { readAnnouncementUserController } from '../controllers/announcements/readAnnouncementUser.controller';
+import { updateAnnouncementController } from '../controllers/announcements/updateAnnouncement.controller';
+import isOwnerResource from '../middlewares/isOwnerResource.middleware';
+import verifyAuthTokenMiddleware from '../middlewares/verifyAuthToken.middleware';
+import { prisma } from '../prisma';
 const routes = Router();
 
 export const announcementRoutes = () => {
-  routes.get("", readAnnouncementController);
-  routes.get("/:id", readAnnouncementIdController);
-  routes.get("/user/:id", readAnnouncementUserController);
-  routes.post("", verifyAuthTokenMiddleware, createAnnouncementController);
-  routes.patch("/:id", verifyAuthTokenMiddleware, updateAnnouncementController);
-  routes.delete(
-    "/:id",
+  routes.get('', readAnnouncementController);
+  routes.get('/:id', readAnnouncementIdController);
+  routes.get('/user/:id', readAnnouncementUserController);
+  routes.post('', verifyAuthTokenMiddleware, createAnnouncementController);
+  routes.patch(
+    '/:id',
     verifyAuthTokenMiddleware,
+    isOwnerResource(prisma.announcement),
+    updateAnnouncementController
+  );
+  routes.delete(
+    '/:id',
+    verifyAuthTokenMiddleware,
+    isOwnerResource(prisma.announcement),
     deleteAnnouncementController
   );
 
