6da96f8dd0998f0249e25e647ad818cf484139e0
Rodrigo Nunes <imrodrigon@gmail.com>
2023-02-22 23:11:34 -0400

    refactor: update announcement

--- src/services/announcements/updateAnnouncement.service.ts
+++ src/services/announcements/updateAnnouncement.service.ts
@@ -1,63 +1,40 @@
-import { prisma } from "../../prisma";
+import { IAnnouncementUpdate } from '../../interfaces/announcement';
+import { prisma } from '../../prisma';
 
-export const updateAnnouncementService = async (data: any, id: string) => {
-  const { images, ...announcementData } = data;
+export const updateAnnouncementService = async (data: IAnnouncementUpdate, id: string) => {
+  const { images, ...rest } = data;
 
-  const announcement_data: any = await prisma.announcement.findFirst({
-    where: { id: id },
-    include: {
-      images: true,
-    },
-  });
-
-  const image_data = announcement_data.images;
-
-  const images_updated: any = handleAnnouncementDataAndImages(
-    images,
-    image_data
-  );
-
-  const announcement = await prisma.announcement.update({
-    data: {
-      ...announcementData,
-      images: {
-        updateMany: (images_updated ?? [])
-          .slice(0, 3)
-          .map((image: any, index: any) => ({
-            where: { id: images_updated[index].id },
-            data: { image_url: images![index]?.image_url },
-          })),
+  if (images) {
+    const announcement = await prisma.announcement.update({
+      data: {
+        ...rest,
+        images: {
+          deleteMany: {},
+          create: data.images,
+        },
       },
-    },
-    where: { id },
-    include: { images: true },
-  });
+      where: {
+        id,
+      },
+      include: {
+        images: true,
+      },
+    });
 
-  return announcement;
-};
+    return announcement;
+  } else {
+    const announcement = await prisma.announcement.update({
+      data: {
+        ...rest,
+      },
+      where: {
+        id,
+      },
+      include: {
+        images: true,
+      },
+    });
 
-function handleAnnouncementDataAndImages(images: any, image_data: any) {
-  const images_updated: Array<{
-    id: string;
-    image_url: string;
-    announcement_id: string;
-  }> = [];
-
-  let x = 0;
-  for (let y = 0; y <= 2; y++) {
-    let image = image_data[y];
-    for (let i = 0; i <= 2; i++) {
-      if (i == x) {
-        const updated = {
-          id: image.id,
-          image_url: images![i]?.image_url,
-          announcement_id: image.announcement_id,
-        };
-        images_updated.push(updated);
-      }
-    }
-    x++;
+    return announcement;
   }
-
-  return images_updated;
-}
+};
