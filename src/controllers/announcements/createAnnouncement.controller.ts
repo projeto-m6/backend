eba9583b8f9abb27c2ac2c3dd49073302325624f
Rodrigo Nunes <imrodrigon@gmail.com>
2023-02-22 23:10:55 -0400

    feat: create logic for advertiser

--- src/controllers/announcements/createAnnouncement.controller.ts
+++ src/controllers/announcements/createAnnouncement.controller.ts
@@ -1,12 +1,16 @@
-import { Request, Response } from "express";
-import { createAnnouncementService } from "../../services/announcements/createAnnouncement.service";
+import { Request, Response } from 'express';
+import { createAnnouncementService } from '../../services/announcements/createAnnouncement.service';
 
-export const createAnnouncementController = async (
-  req: Request,
-  res: Response
-) => {
+export const createAnnouncementController = async (req: Request, res: Response) => {
   const data = req.body;
-  const { id } = req.user;
+  const { id, is_buyer } = req.user;
+
+  /**
+   * Se is_buyer for true significa que o usuario é comprador e apenas anunciantes podem criar um anúncio
+   */
+  if (is_buyer) {
+    return res.status(401).json({ message: 'Only advertiser can create announcement' });
+  }
 
   const announcement = await createAnnouncementService(data, id);
 
