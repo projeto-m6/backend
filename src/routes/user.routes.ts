53967fbc5acc69c73486d13818c84af66b99f197
Marcelo Kodaira <marcelo.aallmeida@gmail.com>
2023-02-27 11:47:58 -0300

    feat/updateUser - route/controller/service

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import { createUserController } from '../controllers/users/createUser.controller';
+import { updateUserController } from '../controllers/users/updateUser.controller';
+import isOwnerResource from '../middlewares/isOwnerResource.middleware';
+import verifyAuthTokenMiddleware from '../middlewares/verifyAuthToken.middleware';
 
 const routes = Router();
 
 export const userRoutes = () => {
   routes.post('', createUserController);
+  routes.patch('/:id',verifyAuthTokenMiddleware,isOwnerResource, updateUserController)
 
   return routes;
 };
