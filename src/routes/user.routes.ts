f75b7782d2846374816358bf0165dd0cfe6b5393
Marcelo Kodaira <marcelo.aallmeida@gmail.com>
2023-02-27 13:50:10 -0300

    feat/isOwnUserMiddleware - routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { createUserController } from '../controllers/users/createUser.controller';
 import { updateUserController } from '../controllers/users/updateUser.controller';
+import { isOwnUserMiddleware } from '../middlewares/isOwnUser.middleware';
 import verifyAuthTokenMiddleware from '../middlewares/verifyAuthToken.middleware';
 
 
@@ -8,8 +9,8 @@ const routes = Router();
 
 export const userRoutes = () => {
   routes.post('', createUserController);
-  routes.patch('/:id',verifyAuthTokenMiddleware, updateUserController)
-  routes.delete('/:id',verifyAuthTokenMiddleware, updateUserController)
+  routes.patch('/:id',verifyAuthTokenMiddleware, isOwnUserMiddleware, updateUserController)
+  routes.delete('/:id',verifyAuthTokenMiddleware, isOwnUserMiddleware, updateUserController)
 
   return routes;
 };
