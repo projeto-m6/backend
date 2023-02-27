cdd4e3bc4cfa1cbe0127902536e47625b806be7c
Marcelo Kodaira <marcelo.aallmeida@gmail.com>
2023-02-27 13:36:13 -0300

    feat/deleteUser - routes/controller/service

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,12 +3,14 @@ import { createUserController } from '../controllers/users/createUser.controller
 import { updateUserController } from '../controllers/users/updateUser.controller';
 import isOwnerResource from '../middlewares/isOwnerResource.middleware';
 import verifyAuthTokenMiddleware from '../middlewares/verifyAuthToken.middleware';
+import { prisma } from '../prisma';
 
 const routes = Router();
 
 export const userRoutes = () => {
   routes.post('', createUserController);
-  routes.patch('/:id',verifyAuthTokenMiddleware,isOwnerResource, updateUserController)
+  routes.patch('/:id',verifyAuthTokenMiddleware, updateUserController)
+  routes.delete('/:id',verifyAuthTokenMiddleware, updateUserController)
 
   return routes;
 };
