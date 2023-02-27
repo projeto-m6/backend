01ad6533dbccaaf3718b46022b532b3ff53986ad
Marcelo Kodaira <marcelo.aallmeida@gmail.com>
2023-02-27 13:46:49 -0300

    feat: isOwnUser Middleware

---
+++ src/middlewares/isOwnUser.middleware.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+
+export const isOwnUserMiddleware = (req: Request, res: Response, next: NextFunction) =>{
+
+    const {id} = req.params
+    const userId = req.user.id
+
+    if(id === userId){
+        next()
+    }
+
+    return res.status(403).json({ message: "You cannot change this user!" })
+}
